c4fffbe64ec0680e017aa019d1f6754bacf1788f
Mayconn Oliveira <mayconnwgo@gmail.com>
2019-09-10 23:56:39 -0300

    Start initial page

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,7 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
+import { MdMenu, MdApps } from "react-icons/md";
+import logo from "../../assets/png/logo.png";
+import questionMark from "../../assets/svg/question.svg";
+import {
+  Container,
+  Header,
+  Content,
+  Aside,
+  Section,
+  Nav,
+  WriteButton
+} from "./style";
 
 function App() {
-  return <h1>Main</h1>;
+  const [account, setAccount] = useState({
+    initial: "M"
+  });
+  return (
+    <Container>
+      <Header>
+        <div className="leftSide">
+          <MdMenu size={24} />
+          <img src={logo} alt="Logo Gmail" />
+        </div>
+        <div className="center">
+          <input type="text" placeholder="Pesquisar e-mail" />
+        </div>
+        <div className="rightSide">
+          <img
+            className="questionMark"
+            src={questionMark}
+            alt="Question mark"
+          />
+          <MdApps className="moreApps" size={24} />
+          <span>{account.initial}</span>
+        </div>
+      </Header>
+      <Content>
+        <Aside>
+          <div className="top">
+            <WriteButton>Escrever</WriteButton>
+          </div>
+          <div className="bottom">
+            <img src={questionMark} alt="Question mark" />
+            <img src={questionMark} alt="Question mark" />
+            <img src={questionMark} alt="Question mark" />
+          </div>
+        </Aside>
+        <Section>Meio</Section>
+        <Nav>Icones</Nav>
+      </Content>
+    </Container>
+  );
 }
 
 export default App;
